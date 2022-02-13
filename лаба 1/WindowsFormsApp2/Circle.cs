1fa5044e56e006c27e2f9f2e3c0037c48e3eea06
dianazmitrevich <zmitrevichdiana@gmail.com>
2022-02-13 12:25:31 +0300

    created circle base class

---
+++ лаба 1/WindowsFormsApp2/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class Circle
+    {
+        public int Radius { get; set; }
+
+        public Circle (int radius = 0)
+        {
+            this.Radius = radius;
+        }
+
+        public override string ToString()
+        {
+            return $"Радиус круга = {this.Radius}";
+        }
+    }
+}
