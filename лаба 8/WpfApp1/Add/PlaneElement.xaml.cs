36fa44da44a07d23ef8a8a55b573cb907a1ce57d
dianazmitrevich <zmitrevichdiana@gmail.com>
2022-04-11 00:56:22 +0300

    binded datagrid to form data

--- лаба 8/WpfApp1/Add/PlaneElement.xaml.cs
+++ лаба 8/WpfApp1/Add/PlaneElement.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,56 @@ namespace WpfApp1.Add
     /// </summary>
     public partial class PlaneElement : UserControl
     {
+        string connectionString = @"Data Source=DESKTOP-G5D9MGO;Initial Catalog=Airport;Integrated Security=True;MultipleActiveResultSets=True;";
+        string radioButtonModel;
+
         public PlaneElement()
         {
             InitializeComponent();
+        }
+
+        private void rbAirbus_Checked(object sender, RoutedEventArgs e) => radioButtonModel = "Airbus";
+        private void rbBoeing_Checked(object sender, RoutedEventArgs e) => radioButtonModel = "Boeing";
+
+        private void AddPlaneButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridPlanes.Items.Clear();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    SqlCommand command = connection.CreateCommand();
+                    command.Transaction = transaction;
+
+                    command.CommandText = "INSERT INTO PLANE (ID, Type, Model, Capacity, Year, Load_Capacity, Maintenance_Date, Manufacturer_ID) VALUES (" + Convert.ToInt32(planeID.Text) + ", '" + Convert.ToString(planeType.Text) + "', '" + radioButtonModel + "', " + Convert.ToInt32(planeCapacity.Text) + ", " + Convert.ToInt32(planeYear.Text) + ", '" + planeLoadCapacity.Text + "', '" + Convert.ToDateTime(planeMDate.Text) + "', NULL)";
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                    command.CommandText = "SELECT * FROM PLANE";
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string type = reader.GetString(1);
+                            string model = reader.GetString(2);
+                            int capacity = reader.GetInt32(3);
+                            int year = reader.GetInt32(4);
+                            string loadCapacity = reader.GetString(5);
+                            DateTime maintenance = reader.GetDateTime(6);
+                            Plane plane = new Plane(id, type, model, capacity, year, loadCapacity, maintenance, 000);
+                            DataGridPlanes.Items.Add(plane);
+                        }
+                    }
+                    MessageBox.Show("Данные добавлены", "Добавление данных", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show($"Ошибка: {x.Message}", "Сообщение об ошибке", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
---
+++ лаба 8/WpfApp1/Plane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Plane
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public int Year { get; set; }
+        public string LoadCapacity { get; set; }
+        public DateTime Maintenance { get; set; }
+        public int Manufacturer { get; set; }
+
+        public Plane(int id, string type, string model, int capacity, int year, string loadCapacity,
+            DateTime maintenance, int manufacturer)
+        {
+            this.ID = id;
+            this.Type = type;
+            this.Model = model;
+            this.Capacity = capacity;
+            this.Year = year;
+            this.LoadCapacity = loadCapacity;
+            this.Maintenance = maintenance;
+            this.Manufacturer = manufacturer;
+        }
+    }
+}
